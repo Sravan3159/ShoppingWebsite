929dfe040086edaa5e0c66734d9c5a3501721984
sravan <malikantisravan123@gmail.com>
2022-10-10 13:49:32 +0530

    adding all files

---
+++ ShoppingWebsite/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using ShoppingWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ShoppingWebsite.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        ShoppingEntities db = new ShoppingEntities();
+
+        // GET: Account
+        
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            bool isvalid = db.Users.Any(x => x.username == user.username && x.password == user.password);
+            if(isvalid)
+            {
+                FormsAuthentication.SetAuthCookie(user.username, false);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid username and password");
+                return View();
+
+            }
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(User user)
+        {
+            db.Users.Add(user);
+            db.SaveChanges();
+            return RedirectToAction("Login");
+
+        }
+    }
+}
---
+++ ShoppingWebsite/dbo.Table_2.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CategoryId] VARCHAR(50) NOT NULL, 
+    [BrandId] VARCHAR(50) NOT NULL, 
+    [Quantity] DECIMAL(18, 2) NOT NULL, 
+    [Price] DECIMAL(18, 2) NOT NULL
+)
